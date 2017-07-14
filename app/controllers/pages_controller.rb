64dceddc3bd47fe18983b0e89d361fc012ac1eb6
Laurent Milne <lmilne360@gmail.com>
2017-07-13 21:09:45 -0400

    results styling update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,13 @@ class PagesController < ApplicationController
   end
 
   def search_gems
+    result = Gems.search(params["search-term"]).select { |e| e['name'] == params['search-term'] }
+
+    if result.blank?
+       @error = "Oh no! Looks like that gem can't be found."
+    else
+       @gem = GemService.new(result[0]).create_gem
+    end
+    render :search
   end
 end
